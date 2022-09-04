8ea492f592c3641a138dbb2587c59eee321c9669
Aakriti yadav <aakritiyadav0108@gmail.com>
2022-09-04 21:49:41 +0530

    MRPEDITTASK6

--- MRPASSIGNTASK6.js
+++ MRPASSIGNTASK6.js
@@ -30,26 +30,87 @@ function deletepost(post)
     {
         setTimeout(()=>{
             if(posts.length!=0){
-                resolve(posts.pop());
+                posts.pop();
+                getedited();
+                resolve();
         }
          else{
             reject('array is empty')
            
          }
-        })
+        },1000)
         })
 }
 
  createedit({title:'post three'})
+ createedit({title:'post four'})
 .then(getedited)
 .then(deletepost)
 .then(deletepost)
 .then(deletepost)
-.then(deletepost)
-.then(getedited)
-.then(deletepost)
+
 
 .catch( err=> console.log(err));
-createedit({title:'post four'})
 
 
+
+// Promise two
+
+
+
+// const posts=[{title:'Post One', body:'this is post one'},
+//             {title:'post two', body:'this is post two'}]
+
+//             function getedited(){
+//                 let output='';
+//               posts.forEach((post,index)=>{
+//              output+=`<li> ${post.title}</li>`
+//               })
+//               document.body.innerHTML+=output;
+//             }
+
+//      function createedit(post)
+//      {
+//         return new Promise((resolve,reject)=>{
+//             posts.push(post)
+//             let error=true;
+//             if(!error)
+//             {
+//                 getedited();
+//                 resolve('going')
+               
+//             }
+//             else{
+//                 reject('Error:Something went wrong')
+//             }
+//         })
+//      }
+     
+// // const promise1= new Promise('hello world');
+// // const promise2= 10;
+// // const promise3=new Promise((resolve,reject)=>{
+// //     (resolve,'goodbye')})     
+// //     Promise.all([promise1,promise2,promise3]).then(values=> console.log(values))
+// const user ={
+//     name:'aakriti',
+//    meettime :'13th august'
+// }
+// function lastedtime()
+// {
+//     return new Promise((resolve,reject)=>{
+       
+//         user.meettime=new Date().getTime();
+
+        
+//         resolve(user.meettime)
+//         reject('want wrong')
+        
+//     })
+// }
+
+
+// let edited=createedit({title:'post three'})
+// let updated=lastedtime();
+// Promise.all([edited,updated])
+// .then(get=>console.log())
+// .catch(err=>console.log(err))
